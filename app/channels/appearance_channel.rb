8ee1914779a2fd3c3f7a4fb07ca18c3e3c267960
occmium <occkart@yahoo.com>
2019-11-05 18:32:40 +0600

    Task_73-1 who is online

---
+++ app/channels/appearance_channel.rb
@@ -0,0 +1,21 @@
+class AppearanceChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "appearance_channel"
+
+    if current_user
+      ActionCable.server.broadcast "appearance_channel",
+                                   { user: current_user.id, online: :on }
+      current_user.online = true
+      current_user.save!
+    end
+  end
+
+  def unsubscribed
+    if current_user
+      ActionCable.server.broadcast "appearance_channel",
+                                   { user: current_user.id, online: :off }
+      current_user.online = false
+      current_user.save!
+    end
+  end
+end
